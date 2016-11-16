5fb24e0e82effde7e62c1d69277e30f1c5c1ecd6
Federico Lois <federico.lois@corvalius.com>
2016-11-16 11:08:34 -0300

    Updated the build process and removed unused files.

--- Scripts/ManageBuildNumbers.ps1
+++ Scripts/ManageBuildNumbers.ps1
@@ -12,8 +12,10 @@ function Update-BuildVersionFromRevision
     $rawVersionNumberGroup = get-content $foundFiles | select-string -pattern $assemblyVersionPattern | select -first 1 | % { $_.Matches }              
     $rawVersionNumber = $rawVersionNumberGroup.Groups[1].Value  
                     
-    $versionParts = $rawVersionNumber.Split('.')       
-    $updatedAssemblyVersion = "{0}.{1}.{2}" -f $versionParts[0], $versionParts[1], $versionParts[2]
+    $versionParts = $rawVersionNumber.Split('.')     
+    [int]$currentVersion = 0;  
+    [bool]$result = [int]::TryParse($versionParts[2], [ref]$currentVersion)
+    $updatedAssemblyVersion = "{0}.{1}.{2}" -f $versionParts[0], $versionParts[1], ($currentVersion+1)
       
     $assemblyVersion  
                   
